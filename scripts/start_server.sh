8a62e3dc35a1e4705df6820ecd747fddd43e960a
MVennaEpam <vennamurthy@gmail.com>
2026-01-18 22:00:11 +0530

    Make CodeDeploy scripts executable

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
+set -e
 echo "Starting session-api service..."
-
-systemctl daemon-reload
-systemctl start session-api
-systemctl status session-api --no-pager
+systemctl restart session-api
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-echo "Stopping session-api service if running..."
 systemctl stop session-api || true
